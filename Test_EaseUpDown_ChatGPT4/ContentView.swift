04d06f47b6040e95e41cb6e9fdbb70d25e60e7b4
Tom Flahive <TomPFlahive@gmail.com>
2023-03-24 15:16:57 -0700

    Commit initial code

--- Test_EaseUpDown_ChatGPT4/ContentView.swift
+++ Test_EaseUpDown_ChatGPT4/ContentView.swift
@@ -1,21 +1,43 @@
-//
-//  ContentView.swift
-//  Test_EaseUpDown_ChatGPT4
-//
-//  Created by Tom Flahive on 3/21/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var expandedRow: Int? = nil
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List {
+            ForEach(0..<10, id: \.self) { index in
+                VStack {
+                    HStack {
+                        Text("Row \(index)")
+                        Spacer()
+                    }
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        if expandedRow == index {
+                            expandedRow = nil
+                        } else {
+                            expandedRow = index
+                        }
+                    }
+
+                    if expandedRow == index {
+                        DetailView()
+                            //.transition(.slide)
+                            //.animation(.easeInOut(duration: 1), value: expandedRow)
+                    }
+                }
+            }
         }
-        .padding()
+    }
+}
+
+struct DetailView: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 10)
+            .fill(Color(red: 0.9, green: 0.9, blue: 1.0))
+            .frame(height: 100)
+            //.padding(.top, 8)
+            //.padding([.leading, .trailing], 6)            
     }
 }
 
